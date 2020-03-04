2e08deda03362112667efd799e8a2345e705a28e
Furqan Safdar <furqan.safdar@gmail.com>
2020-03-04 22:44:42 +0300

    Corrections in initial commit
    Corrections in initial commit

--- FSX.Wpf.Toolkit/Properties/AssemblyInfo.cs
+++ FSX.Wpf.Toolkit/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Markup;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FSX.Wpf.Toolkit")]
-[assembly: AssemblyDescription("Furqan Safdar")]
+[assembly: AssemblyDescription("WPF Toolkit and Controls Library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("FSX Technologies")]
+[assembly: AssemblyCompany("Furqan Safdar")]
 [assembly: AssemblyProduct("FSX.Wpf.Toolkit")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
@@ -31,13 +30,13 @@ using System.Windows;
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
 
-[assembly:ThemeInfo(
+[assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
-                             //(used if a resource is not found in the page,
-                             // or application resource dictionaries)
+                                     //(used if a resource is not found in the page,
+                                     // or application resource dictionaries)
     ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
-                                      //(used if a resource is not found in the page,
-                                      // app, or any theme specific resource dictionaries)
+                                              //(used if a resource is not found in the page,
+                                              // app, or any theme specific resource dictionaries)
 )]
 
 
@@ -51,5 +50,9 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
+
+[assembly: XmlnsPrefix("http://schema.fsxtechnologies.com/xaml", "fsx")]
+[assembly: XmlnsDefinition("http://schema.fsxtechnologies.com/xaml", "FSX.Wpf.Toolkit")]
+[assembly: XmlnsDefinition("http://schema.fsxtechnologies.com/xaml", "FSX.Wpf.Toolkit.Controls")]
